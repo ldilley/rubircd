c4a93bab9f54a73804fc48a3e24744b946100c95
Lloyd Dilley <ldilley@users.noreply.github.com>
2016-11-07 20:09:09 -0500

    Added certificate authority support for SSL/TLS which resolves #41. CAP ACK and NAK bug squashed which fixes #45.

--- tools/create_certificate.sh
+++ tools/create_certificate.sh
@@ -2,4 +2,6 @@
 openssl req -x509 -nodes -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365
 if [ -f key.pem ]; then
   chmod 400 key.pem
+  printf "\nPlace cert.pem and key.pem in the cfg directory and enable SSL/TLS in options.yml if desired.\n"
+  printf "Also, you may optionally include ca.pem (if you have one) within the same directory for additional validation.\n"
 fi
